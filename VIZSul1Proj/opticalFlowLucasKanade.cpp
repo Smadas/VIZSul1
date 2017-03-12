dc11c121911e5b0bc2bda840a71187a7a01ebc4b
Smadas <adam.sojka@smadas.net>
2017-03-12 16:42:18 +0100

    added demo function to opticalFlowLucasKanade library

--- VIZSul1Proj/opticalFlowLucasKanade.cpp
+++ VIZSul1Proj/opticalFlowLucasKanade.cpp
@@ -6,120 +6,62 @@
 #include <iostream>
 #include <ctype.h>
 
+#include "readImagesFromDirectory.h"
+
 using namespace cv;
-using namespace std;
 
-Point2f point;
-bool addRemovePt = false;
+int computeOpticalFlow(cv::Mat gray, cv::Mat prevGray, std::vector<cv::Point2f> points, std::vector<cv::Point2f> prevPoints) {
+	cv::TermCriteria termcrit(cv::TermCriteria::COUNT | cv::TermCriteria::EPS, 20, 0.03);
+	Size subPixWinSize(10, 10), winSize(31, 31);
 
-int computeOpticalFlow() {
+	const int MAX_COUNT = 500;
+
+	if (!points.empty()){
+		std::vector<uchar> status;
+		std::vector<float> err;
+		if (prevGray.empty())
+			gray.copyTo(prevGray);
+		calcOpticalFlowPyrLK(prevGray, gray, points, prevPoints, status, err, winSize,
+			3, termcrit, 0, 0.001);
+	}
+
+	std::swap(prevPoints, points);
+	cv::swap(prevGray, gray);
+
 	return 0;
 }
 
-int main(int argc, char** argv)
-{
-	VideoCapture cap;
-	TermCriteria termcrit(TermCriteria::COUNT | TermCriteria::EPS, 20, 0.03);
-	Size subPixWinSize(10, 10), winSize(31, 31);
-
-	const int MAX_COUNT = 20;
-	bool needToInit = false;
-	bool nightMode = false;
-
-
-	cap.open(0);
-
-
-	if (!cap.isOpened())
-	{
-		cout << "Could not initialize capturing...\n";
-		return 0;
-	}
+int showOpticalFlow() {
+	//read image files
+	std::vector<cv::Mat> readImages = readImgFiles("captureVidX\\");
+	
 
 	namedWindow("LK Demo", 1);
 
 	Mat gray, prevGray, image, frame;
-	vector<Point2f> points[2];
+	std::vector<Point2f> points[2];
 
-	for (;;)
-	{
-		cap >> frame;
+	for (int i = 0; i < readImages.size(); i++){
+		std::cout << "autobus" << std::endl;
+		frame = readImages.at(i);
 		if (frame.empty())
 			break;
 
 		frame.copyTo(image);
 		cvtColor(image, gray, COLOR_BGR2GRAY);
-		
-		if (nightMode)
-			image = Scalar::all(0);
 
-		if (needToInit)
+		computeOpticalFlow(gray, prevGray, points[0], points[1]);
+		if (!points[0].empty())
 		{
-			// automatic initialization
-			goodFeaturesToTrack(gray, points[1], MAX_COUNT, 0.01, 10, Mat(), 3, 0, 0.04);
-			cornerSubPix(gray, points[1], subPixWinSize, Size(-1, -1), termcrit);
-			addRemovePt = false;
-		}
-		else if (!points[0].empty())
-		{
-			vector<uchar> status;
-			vector<float> err;
-			if (prevGray.empty())
-				gray.copyTo(prevGray);
-			calcOpticalFlowPyrLK(prevGray, gray, points[0], points[1], status, err, winSize,
-				3, termcrit, 0, 0.001);
 			size_t i, k;
 			for (i = k = 0; i < points[1].size(); i++)
 			{
-				if (addRemovePt)
-				{
-					if (norm(point - points[1][i]) <= 5)
-					{
-						addRemovePt = false;
-						continue;
-					}
-				}
-
-				if (!status[i])
-					continue;
-
-				points[1][k++] = points[1][i];
 				circle(image, points[1][i], 3, Scalar(0, 255, 0), -1, 8);
 			}
-			points[1].resize(k);
 		}
 
-		if (addRemovePt && points[1].size() < (size_t)MAX_COUNT)
-		{
-			vector<Point2f> tmp;
-			tmp.push_back(point);
-			cornerSubPix(gray, tmp, winSize, Size(-1, -1), termcrit);
-			points[1].push_back(tmp[0]);
-			addRemovePt = false;
-		}
-
-		needToInit = false;
-		imshow("LK Demo", image);
-
-		char c = (char)waitKey(10);
-		if (c == 27)
-			break;
-		switch (c)
-		{
-		case 'r':
-			needToInit = true;
-			break;
-		case 'c':
-			points[0].clear();
-			points[1].clear();
-			break;
-		case 'n':
-			nightMode = !nightMode;
-			break;
-		}
-
-		std::swap(points[1], points[0]);
-		cv::swap(prevGray, gray);
+		cv::imshow("LK Demo", image);
+		cv::waitKey(20);
 	}
 
 	return 0;
